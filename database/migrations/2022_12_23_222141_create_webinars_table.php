99cd6f3a1d7beb640d4966275e5dce83fe1d0a7c
itWelt <rassulovmurad@gmail.com>
2022-12-24 01:32:14 +0300

    [main][task] added tables of webinars and gifts

---
+++ database/migrations/2022_12_23_222141_create_webinars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebinarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webinars', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('uuid')->index();
+            $table->string('name');
+            $table->string('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webinars');
+    }
+}
---
+++ database/migrations/2022_12_23_222510_create_gifts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gifts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('uuid')->index();
+            $table->string('name');
+            $table->string('link');
+            $table->integer('number');
+            $table->integer('delay');
+            $table->foreignId('webinar_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gifts');
+    }
+}
