6f03519a77e764a10e6704fd030fafac5bdb5c81
itWelt <rassulovmurad@gmail.com>
2022-12-24 02:07:05 +0300

    [main][task] added webibar CRUD prototype

--- app/Http/Controllers/Api/WebinarController.php
+++ app/Http/Controllers/Api/WebinarController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Webinar\WebinarCreateRequest;
+use App\Http\Requests\Api\Webinar\WebinarUpdateRequest;
+use App\Http\Resources\Webinar\WebinarResource;
+use App\Http\Resources\Webinar\WebinarsResource;
+use App\Models\Webinar;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class WebinarController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return WebinarsResource::collection(Webinar::all());
+    }
+    public function create(WebinarCreateRequest $request)
+    {
+        $webinar = Webinar::createWebinar($request->all());
+
+        return $webinar ? $webinar->uuid : null;
+    }
+    public function get(Webinar $webinar): WebinarResource
+    {
+        return new WebinarResource($webinar);
+    }
+    public function update(Webinar $webinar, WebinarUpdateRequest $request)
+    {
+        $webinar->update($request->all());
+
+        return response()->json(['message' => 'success by update'], Response::HTTP_OK);
+    }
+    public function delete(Webinar $webinar)
+    {
+        $webinar->delete();
+
+        return response()->json(['message' => 'success by delete'], Response::HTTP_OK);
+    }
 }
---
+++ app/Http/Resources/Webinar/WebinarsResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Webinar;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WebinarsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
