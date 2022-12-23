843151b8f30ffcd06c4b12bc312c80276915fec1
itWelt <rassulovmurad@gmail.com>
2022-12-24 01:58:37 +0300

    [main][task] fixation commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\WebinarController;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('embedded')->group(function () {});
@@ -9,6 +10,14 @@ Route::prefix('v1')->group(function () {
     Route::prefix('admin')->middleware('token')->group(function () {
         Route::prefix('auth')->group(function () {
             Route::post('/signin', [AuthController::class, 'signin'])->withoutMiddleware('token');
+        });
+
+        Route::prefix('mortgages')->group(function () {
+            Route::get('/', [WebinarController::class, 'index']);
+            Route::post('/', [WebinarController::class, 'create']);
+            Route::get('/{mortgage:uuid}', [WebinarController::class, 'get']);
+            Route::put('/{mortgage:uuid}/update', [WebinarController::class, 'update']);
+            Route::delete('/{mortgage:uuid}/delete', [WebinarController::class, 'delete']);
         });
     });
 });
