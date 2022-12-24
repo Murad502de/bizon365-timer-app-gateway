9e34d34fdfa165b6b54ab8948e0a3e769ba7fe19
itWelt <rassulovmurad@gmail.com>
2022-12-24 13:01:28 +0300

    [main][task] created Api/GiftController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\WebinarController;
+use App\Http\Controllers\Api\GiftController;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('embedded')->group(function () {});
@@ -18,6 +19,14 @@ Route::prefix('v1')->group(function () {
             Route::get('/{webinar:uuid}', [WebinarController::class, 'get']);
             Route::put('/{webinar:uuid}', [WebinarController::class, 'update']);
             Route::delete('/{webinar:uuid}', [WebinarController::class, 'delete']);
+        });
+
+        Route::prefix('gifts')->group(function () {
+            Route::get('/', [GiftController::class, 'index']);
+            Route::post('/', [GiftController::class, 'create']);
+            Route::get('/{gift:uuid}', [GiftController::class, 'get']);
+            Route::put('/{gift:uuid}', [GiftController::class, 'update']);
+            Route::delete('/{gift:uuid}', [GiftController::class, 'delete']);
         });
     });
 });
