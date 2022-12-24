717d8c78a4274bfda5c3b205d22a745af28bed1b
itWelt <rassulovmurad@gmail.com>
2022-12-24 14:23:09 +0300

    [main][task] implement crud of gift  model

--- app/Models/Gift.php
+++ app/Models/Gift.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Traits\Model\generateUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
 
 class Gift extends Model
 {
@@ -43,19 +42,13 @@ class Gift extends Model
     }
     public function updateGift(array $gift)
     {
-        Log::info(__METHOD__, $gift); //DELETE
-
         if (isset($gift['webinar_uuid'])) {
-            Log::info(__METHOD__, ['have uuid']); //DELETE
-
             $webinar = Webinar::getByUuid($gift['webinar_uuid']);
 
             return $this->update(array_merge($gift, [
                 'webinar_id' => $webinar ? $webinar->id : null,
             ]));
         }
-
-        Log::info(__METHOD__, ['dont have uuid']); //DELETE
 
         return $this->update($gift);
     }
