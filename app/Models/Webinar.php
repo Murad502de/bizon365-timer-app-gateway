fc8d1f2b986fb96cb0686fc0a1d5cf9e7b63c15d
itWelt <rassulovmurad@gmail.com>
2022-12-24 12:36:28 +0300

    [main][task] edit Webinar model

--- app/Models/Webinar.php
+++ app/Models/Webinar.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Webinar extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'name',
+        'code',
+    ];
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }
